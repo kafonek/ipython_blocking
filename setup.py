c1527bf3a9da9693ba06f88018c2c443953f458a
kafonek <kafonek@gmail.com>
2019-04-13 12:01:49 -0400

    restructured project metadata

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='ipython_blocking',
+      version='0.2.0',
+      
+      author='Matt Kafonek',
+      author_email='kafonek@gmail.com',
+      url='https://github.com/kafonek/ipython_blocking',
+            
+      description='Context manager for blocking cell execution within a Jupyter notebook',
+      long_description=open('README.md', encoding='utf-8').read(),
+      
+      packages=find_packages(),
+      install_requires=['IPython', 'ipywidgets'],
+      
+      classifiers = ['Framework :: IPython',
+                     'Programming Language :: Python',
+                     'Intended Audience :: Developers',
+                     'Development Status :: 3 - Alpha',
+                     ],
+      license='BSD')
+      
