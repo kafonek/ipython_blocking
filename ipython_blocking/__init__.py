7294157bc34724c4cfc9ee32bbf08d52dbafca2c
Matt Kafonek <kafonek@gmail.com>
2021-08-28 11:21:44 -0400

    black/isort formatting

--- ipython_blocking/__init__.py
+++ ipython_blocking/__init__.py
@@ -1,11 +1,11 @@
 from .ipython_blocking import CaptureExecution
 from .ipython_magic import CaptureMagic, load_ipython_extensions
 
-__version__ = '0.3.0'
+__version__ = "0.3.0"
 
 try:
     # This won't load when get_ipython() isn't defined, e.g. in default python shell.
-    # In an IPython/Jupyter environment, I want the %block and %blockrun magic 
+    # In an IPython/Jupyter environment, I want the %block and %blockrun magic
     # to be available by default
     load_ipython_extensions()
 except:
--- ipython_blocking/ipython_blocking.py
+++ ipython_blocking/ipython_blocking.py
@@ -1,7 +1,6 @@
 import sys
 
 import tornado.queues
-
 from nbclient.util import just_run
 
 ### General context manager usage:
@@ -13,47 +12,50 @@ from nbclient.util import just_run
 #             break
 #         ctx.step()
 
+
 class CaptureExecution:
     "A context manager to capture execute_request events then either replay or disregard them after exiting the manager"
+
     def __init__(self, replay=True):
         self.captured_events = []
         self._replay = replay
         self.shell = get_ipython()
         self.kernel = self.shell.kernel
-        
+
     def step(self):
         try:
             just_run(self.kernel.do_one_iteration())
         except tornado.queues.QueueEmpty:
             pass
-    
+
     def capture_event(self, stream, ident, parent):
         "A 'capture' function to register instead of the default execute_request handling"
         self.captured_events.append((stream, ident, parent))
 
     def start_capturing(self):
         "Overwrite the kernel shell handler to capture instead of executing new cell-execution requests"
-        self.kernel.shell_handlers['execute_request'] = self.capture_event
+        self.kernel.shell_handlers["execute_request"] = self.capture_event
 
     def stop_capturing(self):
         "revert the kernel shell handler to the default execute_request behavior"
-        self.kernel.shell_handlers['execute_request'] = self.kernel.execute_request
-    
+        self.kernel.shell_handlers["execute_request"] = self.kernel.execute_request
+
     def replay_captured_events(self):
         "Called at end of context -- replays all captured events once the default execution handler is in place"
         # need to flush before replaying so messages show up in current cell not replay cells
-        sys.stdout.flush() 
+        sys.stdout.flush()
         sys.stderr.flush()
         for stream, ident, parent in self.captured_events:
             # Using kernel.set_parent is the key to getting the output of the replayed events
             # to show up in the cells that were captured instead of the current cell
-            self.kernel.set_parent(ident, parent) 
+            self.kernel.set_parent(ident, parent)
             just_run(self.kernel.execute_request(stream, ident, parent))
-  
+
     def __enter__(self):
         self.start_capturing()
-        self.shell.execution_count += 1 # increment execution count to avoid collision error
-        
+        # increment execution count to avoid collision error
+        self.shell.execution_count += 1
+
     def __exit__(self, exc_type, exc_value, traceback):
         if exc_type is not None:
             # let the error propogate up, such as a keyboard interrupt while capturing cell execution
@@ -61,4 +63,3 @@ class CaptureExecution:
         self.stop_capturing()
         if self._replay:
             self.replay_captured_events()
-
